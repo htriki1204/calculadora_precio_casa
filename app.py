db61d196dc563233e2f1674b993c98ee20a6d346
htriki1204 <htriki@ucm.es>
2024-05-03 12:11:31 +0200

    configura la pagina web para usar el modelo de predicciones de viviendas

---
+++ app.py
@@ -0,0 +1,39 @@
+# app.py
+from flask import Flask, render_template, request, jsonify
+import joblib
+
+app = Flask(__name__)
+
+# Cargar el modelo entrenado
+model = joblib.load("modelo_entrenado.pkl")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.form.to_dict()
+    bedrooms = float(data.get("bedrooms", 0))
+    bathrooms = float(data.get("bathrooms", 0))
+    area = float(data.get("area", 0))
+    year = float(data.get("year", 0))
+    metro = data.get("metro", False)
+    renfe = data.get("renfe", False)
+    air_conditioning = data.get("air_conditioning", False)
+    elevator = data.get("elevator", False)
+    garden = data.get("garden", False)
+    property_type = data.get("property_type", "")
+    etiqueta = data.get("label", "")
+    district = data.get("district", "")
+    print(bedrooms)
+
+    # Preprocesar los datos (si es necesario)
+    # Hacer la predicción
+    prediction = model.predict([[bedrooms, bathrooms, area]])
+    
+    return jsonify({"prediction": prediction[0]})
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
