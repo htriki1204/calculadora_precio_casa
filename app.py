41789724e21bc023a84f400f641f8d90def60817
htriki1204 <htriki@ucm.es>
2024-05-03 17:59:54 +0200

    crea la estructura de datos con la que se va a predecir

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 # app.py
 from flask import Flask, render_template, request, jsonify
+from sklearn.linear_model import LinearRegression 
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler,OneHotEncoder
+import pandas as pd
+
+
 import joblib
 
 app = Flask(__name__)
@@ -25,16 +30,36 @@ def predict():
     elevator = data.get("elevator", False)
     garden = data.get("garden", False)
     property_type = data.get("property_type", "")
+    house_type = data.get("housing_type", "")
     etiqueta = data.get("label", "")
     district = data.get("district", "")
-    print("hola que t")
+    diccionario = {
+        "Dormitorios":bedrooms,
+        "Superficie":area,
+        "Num_baños":bathrooms,
+        "Metro":metro,
+        "Renfe":renfe,
+        "Tipo_de_inmueble":property_type,
+        "Año_de_construccion":year,
+        "Calefaccion":air_conditioning,
+        "Etiqueta":etiqueta,
+        "Aire acondicionado":air_conditioning,
+        "Ascensor":elevator,
+        "Jardin":garden,
+        "Tipo":house_type,
+        "distrito/ciudad":district,
+    }
     print(1)
+    CordenasPorDistritos = pd.read_csv("static/CordenadasPorDistrito.csv")
+    diccionario["Latitud"] = CordenasPorDistritos[CordenasPorDistritos["distrito/ciudad"]=="Alameda de Osuna"]["Latitud"].values
+    diccionario["Longitud"] = CordenasPorDistritos[CordenasPorDistritos["distrito/ciudad"]=="Alameda de Osuna"]["Longitud"].values
+    inputPrediccion = pd.DataFrame(diccionario)
     # Preprocesar los datos (si es necesario)
     # Hacer la predicción
-    #prediction = model.predict([[bedrooms, bathrooms, area]])
+    prediction = model.predict(inputPrediccion)
     
-    #return jsonify({"prediction": prediction[0]})
-    return jsonify({"prediction": 115})
+    return jsonify({"prediction": prediction[0]})
+    #return jsonify({"prediction": 115})
 
 
 if __name__ == "__main__":
